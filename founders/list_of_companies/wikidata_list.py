d1e493d958cd428de12ea30da549f83348bd6093
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2015-04-21 21:53:57 -0700

    New names and stuff

--- founders/list_of_companies/wikidata_list.py
+++ founders/list_of_companies/wikidata_list.py
@@ -3,7 +3,17 @@ import requests
 FILE_PATH = 'wikidata_ids'
 BASE_URL = 'http://wdq.wmflabs.org/api?'
 
-query_string = 'q=CLAIM[31:783794]and%20link[enwiki]'
+query_string = '''q=CLAIM[31:783794]%20or%20\
+                  CLAIM[31:4830453]%20or%20\
+                  CLAIM[31:891723]%20or%20\
+                  CLAIM[31:161726]%20or%20\
+                  CLAIM[31:35127]%20or%20\
+                  CLAIM[31:355]%20or%20\
+                  CLAIM[31:3918]%20or%20\
+                  CLAIM[31:902104]%20or%20\
+                  CLAIM[31:79913]%20or%20\
+                  CLAIM[31:157031]%20and%20\
+                  link[enwiki]'''
 
 r = requests.get(BASE_URL + query_string)
 
