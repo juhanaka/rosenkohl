c2d91b455dca4513718d05c2d4356df8dc363a16
ThomasPalomares <thopalomares@gmail.com>
2015-04-24 12:00:33 -0700

    founder_sentences table in db

---
+++ founders/training_examples/create_table_founder_sentences.sh
@@ -0,0 +1,38 @@
+psql -d deepdive_founder -c """DROP TABLE IF EXISTS founder_company CASCADE;"""
+
+psql -d deepdive_founder -c \
+"""
+CREATE TABLE founder_company(
+  founder text, 
+  company text
+);
+"""
+
+psql -d deepdive_founder -c """DROP TABLE IF EXISTS founder_sentences CASCADE;"""
+
+psql -d deepdive_founder -c \
+"""
+CREATE TABLE founder_sentences(
+  founder text, 
+  company text, 
+  sentence text,
+  sentence_id text
+);
+"""
+
+
+ghead -n -0 founder_company.tsv | psql -d deepdive_founder -c "copy founder_company from STDIN;"
+
+psql -d deepdive_founder -c \
+"""
+INSERT INTO founder_sentences
+SELECT founder, 
+       company, 
+       sentence,
+       sentences.sentence_id
+FROM founder_company, 
+     people_mentions,
+     sentences
+WHERE founder_company.founder = people_mentions.text
+  AND people_mentions.sentence_id = sentences.sentence_id;
+"""
