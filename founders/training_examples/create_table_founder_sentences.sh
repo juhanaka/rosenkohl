8399abf09988e2c78bef9bd2d5d59f9ea8306f7c
ThomasPalomares <thopalomares@gmail.com>
2015-04-24 20:05:14 -0700

    create table founder and company sentences

--- founders/training_examples/create_table_founder_sentences.sh
+++ founders/training_examples/create_table_founder_sentences.sh
@@ -20,6 +20,18 @@ CREATE TABLE founder_sentences(
 );
 """
 
+psql -d deepdive_founder -c """DROP TABLE IF EXISTS company_sentences CASCADE;"""
+
+psql -d deepdive_founder -c \
+"""
+CREATE TABLE company_sentences(
+  company text, 
+  founder text, 
+  sentence text,
+  sentence_id text
+);
+"""
+
 
 ghead -n -0 founder_company.tsv | psql -d deepdive_founder -c "copy founder_company from STDIN;"
 
@@ -35,4 +47,18 @@ FROM founder_company,
      sentences
 WHERE founder_company.founder = people_mentions.text
   AND people_mentions.sentence_id = sentences.sentence_id;
+"""
+
+psql -d deepdive_founder -c \
+"""
+INSERT INTO company_sentences
+SELECT founder_company.company, 
+       founder_company.founder, 
+       sentences.sentence,
+       sentences.sentence_id
+FROM founder_company, 
+     company_mentions,
+     sentences
+WHERE founder_company.company = company_mentions.text
+  AND company_mentions.sentence_id = sentences.sentence_id;
 """
