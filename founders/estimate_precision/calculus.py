fc076d9cd13f2b8d782f84d39d4309179098c5f8
ThomasPalomares <thopalomares@gmail.com>
2015-05-13 00:24:16 -0700

    estimate precision - create graphs

---
+++ founders/estimate_precision/calculus.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+with open('results.txt') as f:
+    for line in f:
+      print line
--- founders/estimate_precision/create_graphs.sh
+++ founders/estimate_precision/create_graphs.sh
@@ -1,3 +1,13 @@
 #! /usr/bin/env bash
 
-psql -d deepdive_founder -c "SELECT count(*) FROM permanent_tags_founder_precision_is_correct WHERE expectation > 0.9 and is_correct='true';" -t >> results.txt
+array=(0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95)
+for i in "${array[@]}"
+do
+  psql -d deepdive_founder -c "SELECT count(*) FROM permanent_tags_founder_precision_is_correct WHERE expectation > $i and is_correct='true';" -t >> results.txt
+done
+
+
+for i in "${array[@]}"
+do
+  psql -d deepdive_founder -c "SELECT count(*) FROM permanent_tags_founder_precision_is_correct WHERE expectation > $i and is_correct='false';" -t >> results.txt
+done
