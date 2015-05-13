a7fd3510152c1d2a40f3a45d0c99752b7b9817b2
ThomasPalomares <thopalomares@gmail.com>
2015-05-13 10:54:33 -0700

    improving estimate results

--- founders/estimate_precision/calculus.py
+++ founders/estimate_precision/calculus.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
-with open('results.txt') as f:
-    for line in f:
-      print line
+expect = [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+
+with open('results.tsv') as f:
+    contents = f.read()
+    arr = map(lambda x: x.strip(), contents.split('\n\n'))
+    arr = [int(x) for x in arr if x != '']
+    print arr
