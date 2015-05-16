cba9df2d2b22192b4a3547f76be928bfb46cc94a
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2015-05-16 10:49:51 -0700

    Family initial commit

---
+++ family/list_of_people/get_ppl_ids_from_wikidata.py
@@ -0,0 +1,26 @@
+import requests
+
+FILE_PATH = 'results/ppl_wikidata_ids'
+BASE_URL = 'http://wdq.wmflabs.org/api?'
+
+query_string = '''q=CLAIM[31:783794]%20or%20\
+                  CLAIM[31:4830453]%20or%20\
+                  CLAIM[31:891723]%20or%20\
+                  CLAIM[31:161726]%20or%20\
+                  CLAIM[31:35127]%20or%20\
+                  CLAIM[31:355]%20or%20\
+                  CLAIM[31:3918]%20or%20\
+                  CLAIM[31:902104]%20or%20\
+                  CLAIM[31:79913]%20or%20\
+                  CLAIM[31:157031]%20and%20\
+                  link[enwiki]'''
+
+r = requests.get(BASE_URL + query_string)
+
+wikidata_ids = r.json()['items']
+
+
+with open(FILE_PATH, 'w') as fp:
+    for wid in wikidata_ids:
+        fp.write(str(wid)+'\n')
+
